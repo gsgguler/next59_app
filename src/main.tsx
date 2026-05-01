868c7cdc4d01facb182461317b530b4a5313bd97
gsgguler <gsgguler@gmail.com>
2026-05-01 04:48:29 +0300

    Updated index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import 'flag-icons/css/flag-icons.min.css';
 
 if ('serviceWorker' in navigator) {
   const isDevEnv = location.hostname === 'localhost'
